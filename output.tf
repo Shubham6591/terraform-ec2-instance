5cef48834248ad5d0ba2b21d08c4be1a2e84220c
Shubham6591 <shubhamgupta6591@gmail.com>
2024-11-23 11:33:49 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,8 @@
+output "instance_id" {
+ value = aws_instance.example.id
+ description = "The ID of the created EC2 instance."
+}
+output "public_ip" {
+ value = aws_instance.example.public_ip
+ description = "The public IP of the created EC2 instance."
+} 
