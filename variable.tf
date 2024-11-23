be3f0847cc138dc8261a640afc2f7dc60d04b331
Shubham6591 <shubhamgupta6591@gmail.com>
2024-11-23 11:36:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+ description = "The Amazon Machine Image (AMI) ID for the EC2 instance"
+ type = string
+
+}
+variable "instance_type" {
+ description = "The type of EC2 instance to launch"
+ type = string
+
+}
+variable "instance_name" {
+ description = "The name of the EC2 instance"
+ type = string
+
+}
